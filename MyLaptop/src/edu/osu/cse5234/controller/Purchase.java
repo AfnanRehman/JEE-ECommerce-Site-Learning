964e8af81b2750b2a675d276d169933cac59c853
mingxinlu <lu.1927@buckeyemail.osu.edu>
2018-09-26 19:28:13 -0400

    123

--- MyLaptop/src/edu/osu/cse5234/controller/Purchase.java
+++ MyLaptop/src/edu/osu/cse5234/controller/Purchase.java
@@ -4,9 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import edu.osu.cse5234.model.*;
 @Controller
 @RequestMapping("/purchase")
 public class Purchase {
@@ -18,7 +19,66 @@ public class Purchase {
 	
 	@RequestMapping(path = "/new", method = RequestMethod.GET)
 	public String printHelloNew() throws Exception {
+		
 		return "HelloJSP";
 		
 	}
+	@RequestMapping(method = RequestMethod.GET)
+	public String viewOrderEntryPage(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		Order order = request.getAttribute("order");
+//		response.getWriter().println("order's name:" + order.)
+		return "OrderEntryForm";
+	}
+
+	
+
+	@RequestMapping(path = "/submitItems", method = RequestMethod.POST)
+	public String submitItems(@ModelAttribute("order") Order order, HttpServletRequest request) {
+		request.getSession().setAttribute("order", order);
+		return "redirect:/purchase/paymentEntry";
+	}
+	
+	@RequestMapping(path = "/paymentEntry", method = RequestMethod.GET)
+	public String viewPaymentEntryPage(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("payment", new PaymentInfo());	
+		return "PaymentEntryForm";
+	}
+	
+	@RequestMapping(path = "/submitPayment", method = RequestMethod.POST)
+	public String  	submitPayment(@ModelAttribute("paymentInfo") PaymentInfo paymentInfo, HttpServletRequest request) {
+		request.getSession().setAttribute("paymentInfo", paymentInfo);
+		return "redirect:/purchase/shippingEntry";
+	}	
+	
+	@RequestMapping(path = "/shippingEntry", method = RequestMethod.GET)
+	public String viewShippingEntryPage(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("payment", new ShippingInfo());	
+		return "ShippingEntryForm";
+	}
+	
+	@RequestMapping(path = "/submitShipping", method = RequestMethod.POST)
+	public String  	submitShipping(@ModelAttribute("shippingInfo") ShippingInfo shippingInfo, HttpServletRequest request) {
+		request.getSession().setAttribute("paymentInfo", shippingInfo);
+		return "redirect:/purchase/viewOrder";
+	}	
+	
+
+	@RequestMapping(path = "/viewOrder", method = RequestMethod.GET)
+	public String viewOrder(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("payment", new Order());	
+		return "viewOrderEntry";
+	}
+	
+	@RequestMapping(path = "/confirmOrder", method = RequestMethod.POST)
+	public String  	confirmOrder(@ModelAttribute("order") Order order, HttpServletRequest request) {
+		request.getSession().setAttribute("order", order);
+		return "redirect:/purchase/viewConfirmation";
+	}
+	
+	@RequestMapping(path = "/viewConfirmation", method = RequestMethod.GET)
+	public String  viewConfirmation(HttpServletRequest request, HttpServletResponse response) {
+//		display order detail.
+		return "redirect:/purchase";
+	}
+	
 }
