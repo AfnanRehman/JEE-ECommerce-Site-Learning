a7cdf5471eabc86a6c3c6ed78295f30a61ddfd71
Afnan <Afnan@172.31.5.26>
2018-09-26 18:47:15 -0400

    step2

--- MyLaptop/src/edu/osu/cse5234/model/Item.java
+++ MyLaptop/src/edu/osu/cse5234/model/Item.java
@@ -1,10 +1,31 @@
 package edu.osu.cse5234.model;
 
 public class Item {
+	private String name = null;
+	private String price = null;
+	private String quantity = null;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Item() { }
+	
+	public String getName() {
+		return name;		
 	}
-
+	
+	public String getPrice() {
+		return price;
+	}
+	
+	public String getQuantity() {
+		return quantity;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
 }
--- MyLaptop/src/edu/osu/cse5234/model/Order.java
+++ MyLaptop/src/edu/osu/cse5234/model/Order.java
@@ -1,5 +1,31 @@
 package edu.osu.cse5234.model;
 
-public class Order implements java.io.Serializable {
-
+public class Order {
+	private String MacBook = null;
+	private String Dell = null;
+	private String Hp = null;
+	
+	public Order() { }
+	
+	public String getMacBook() {
+		return MacBook;		
+	}
+	
+	public String getDell() {
+		return Dell;
+	}
+	
+	public String getHp() {
+		return Hp;
+	}
+	
+	public void setMacBook(String MacBook) {
+		this.MacBook = MacBook;
+	}
+	public void setDell(String Dell) {
+		this.Dell = Dell;
+	}
+	public void setHp(String Hp) {
+		this.Hp = Hp;
+	}
 }
