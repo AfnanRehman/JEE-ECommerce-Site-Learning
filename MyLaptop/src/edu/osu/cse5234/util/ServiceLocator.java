a554ef0622237e32640ed3cbc02c07b7398afc20
Afnan Rehman <afnan.rehman@gmail.com>
2018-10-17 19:05:51 -0400

    Update ServiceLocator.java

--- MyLaptop/src/edu/osu/cse5234/util/ServiceLocator.java
+++ MyLaptop/src/edu/osu/cse5234/util/ServiceLocator.java
@@ -1,27 +1,26 @@
-	package edu.osu.cse5234.util;
+package edu.osu.cse5234.util;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
+import javax.naming.*;
 
 import edu.osu.cse5234.business.OrderProcessingServiceBean;
 import edu.osu.cse5234.business.view.InventoryService;
 
 public class ServiceLocator {
-	
 	public static OrderProcessingServiceBean getOrderProcessingService() {
 		try {
-			return (OrderProcessingServiceBean) InitialContext.doLookup(
+	         return (OrderProcessingServiceBean) InitialContext.doLookup(
 					"java:module/OrderProcessingServiceBean!edu.osu.cse5234.business.OrderProcessingServiceBean");
 		} catch (NamingException ne) {
-			throw new RuntimeException(ne);
+				throw new RuntimeException(ne);
 		}
 	}
+	
 	public static InventoryService getInventoryService() {
 		try {
-			return (InventoryService) InitialContext.doLookup(
-					"\"java:global/MyLaptop-InventoryManagement-EJBEAR/MyLaptop-InventoryManagement-EJB/InventoryServiceBean!edu.osu.cse5234.business.view.InventoryService\"");
+	         return (InventoryService) InitialContext.doLookup(
+	        		 "java:global/MyLaptop-EJBEAR/MyLaptop-EJB/InventoryServiceBean!edu.osu.cse5234.business.view.InventoryService");
 		} catch (NamingException ne) {
-			throw new RuntimeException(ne);
+				throw new RuntimeException(ne);
 		}
 	}
 }
